5972c70262833533bf88d812c021eab47b16e5ef
vinicius-moraisdesouza <moraisdesouzavinicius@gmail.com>
2025-08-16 16:10:35 -0300

    Termino da versão do professor com a funcionalidade de lançar notas

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -48,4 +48,5 @@ urlpatterns = [
     # PROFESSOR - Turmas/Matérias/Notas
     path('professor/materia/<int:materia_id>/turma/<int:turma_id>/', views.ver_turma_professor, name='ver_turma_professor'),
     path('professor/aluno/<int:aluno_id>/detalhes/', views.ver_detalhes_aluno_professor, name='ver_detalhes_aluno_professor'),
+    path('professor/inserir-nota/', views.inserir_nota, name="inserir_nota"),
 ]
