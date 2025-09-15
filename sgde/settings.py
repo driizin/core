8d61296b85aa16fe83a230702cf850e51e82527a
driizin <adrielima1991@gmail.com>
2025-09-15 19:18:49 -0300

    alteração de restaurar o models.py por problemas de mudanças do usuário herdar user do django, mantendo o que tinha antes

--- sgde/settings.py
+++ sgde/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'autenticacao',
-    'core', 
+    'core',
     'materia',
     'turma',
     'professor_materia_turma',
